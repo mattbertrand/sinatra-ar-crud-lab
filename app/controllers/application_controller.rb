ee16954e1c72748945ec97a17afa3cae8ba4560e
github username <github email address>
2020-12-23 11:07:40 -0800

    commit tree d48faa970df95b8bca275fb9bdc675d28d6e6f94

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    find_by_id
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    find_by_id
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    find_by_id
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end
+
+  private
+
+  def find_by_id
+    @article = Article.find(params[:id])
   end
 end
+
+
